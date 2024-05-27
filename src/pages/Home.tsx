98ee4e0938f46c4041916d819cef35965978ada3
Nope777 <suminwow1@gmail.com>
2024-05-27 17:47:44 +0900

    2주차 수정 완료

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,37 +1,39 @@
-import { useEffect, useState } from 'react';
-import { getPostList } from '../api';
+// import { useEffect, useState } from 'react';
+// import { getPostList } from '../api';
 import PostListItem from '../components/PostListItem';
-import { IResponsePostList, TAG } from '../api/types';
+// import { TAG } from '../api/types';
+// import { IResponsePostList, TAG } from '../api/types';
+
 import NoPostList from '../components/NoPostList';
-import Post from './Post.tsx';
+// import Post from './Post.tsx';
 import useGetPostList from '../queries/useGetPostList.ts';
 
-const list = [
-  {
-    post: {
-      id: 1,
-      title: '1번 게시글',
-      contents: '내용',
-      tag: TAG.REACT,
-    },
-  },
-  {
-    post: {
-      id: 2,
-      title: '2번 게시글',
-      contents: '내용',
-      tag: TAG.REACT,
-    },
-  },
-  {
-    post: {
-      id: 3,
-      title: '3번 게시글',
-      contents: '내용',
-      tag: TAG.REACT,
-    },
-  },
-];
+// const list = [
+//   {
+//     post: {
+//       id: 1,
+//       title: '1번 게시글',
+//       contents: '내용',
+//       tag: TAG.REACT,
+//     },
+//   },
+//   {
+//     post: {
+//       id: 2,
+//       title: '2번 게시글',
+//       contents: '내용',
+//       tag: TAG.REACT,
+//     },
+//   },
+//   {
+//     post: {
+//       id: 3,
+//       title: '3번 게시글',
+//       contents: '내용',
+//       tag: TAG.REACT,
+//     },
+//   },
+// ];
 
 const Home = () => {
   // return <div>{/*todo (3-1) post 목록 작성*/}</div>;
